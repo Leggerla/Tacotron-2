d42b8e2b64fe3c7f937d856bdaadfb8ff1ee2f65
Leggera <lana.krlv1995@gmail.com>
2019-06-24 20:06:07 +0300

    Update feeder.py

--- tacotron/feeder.py
+++ tacotron/feeder.py
@@ -105,12 +105,15 @@ class Feeder(threading.Thread):
 		text = meta[5]
 
 		input_data = np.asarray(text_to_sequence(text, self._cleaner_names), dtype=np.int32)
-		mel_folder_num = str(int((meta[1].split('-')[2]).split('.')[0]) // 1000)
-		mel_target = np.load(os.path.join(self._mel_dir, mel_folder_num, meta[1]))
+		num = int((meta[1].split('-')[2]).split('.')[0])
+		folder_num = str(num // 1000)
+		sub_folder_num = str(((num) % 1000) // 100)
+		
+		mel_target = np.load(os.path.join(self._mel_dir, folder_num, sub_folder_num, meta[1]))
 		#Create parallel sequences containing zeros to represent a non finished sequence
 		token_target = np.asarray([0.] * len(mel_target))
-		linear_folder_num = str(int((meta[2].split('-')[2]).split('.')[0]) // 1000)
-		linear_target = np.load(os.path.join(self._linear_dir, linear_folder_num, meta[2]))
+		
+		linear_target = np.load(os.path.join(self._linear_dir, folder_num, sub_folder_num, meta[2]))
 		return (input_data, mel_target, token_target, linear_target, len(mel_target))
 
 
