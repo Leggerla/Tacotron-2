7151e40fb5cce9b02565710b552d1e01fb6d2c3a
Leggera <lana.krlv1995@gmail.com>
2019-07-02 18:15:47 +0300

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -1,136 +1,75 @@
 import tensorflow as tf 
 import numpy as np 
 
-class Map(dict):
-    """
-    Example:
-    m = Map({'first_name': 'Eduardo'}, last_name='Pool', age=24, sports=['Soccer'])
-    """
-    def __init__(self, *args, **kwargs):
-        super(Map, self).__init__(*args, **kwargs)
-        for arg in args:
-            if isinstance(arg, dict):
-                for k, v in arg.items():
-                    self[k] = v
-
-        if kwargs:
-            for k, v in kwargs.items():
-                self[k] = v
-
-    def __getattr__(self, attr):
-        return self.get(attr)
-
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super(Map, self).__setitem__(key, value)
-        self.__dict__.update({key: value})
-
-    def __delattr__(self, item):
-        self.__delitem__(item)
-
-    def __delitem__(self, key):
-        super(Map, self).__delitem__(key)
-        del self.__dict__[key]
 
 # Default hyperparameters
-hparams = Map({
+hparams = tf.contrib.training.HParams(
 	# Comma-separated list of cleaners to run on text prior to training and eval. For non-English
 	# text, you may want to use "basic_cleaners" or "transliteration_cleaners".
-	'cleaners': 'english_cleaners',
+	cleaners='english_cleaners',
 
-	#If you only have 1 GPU or want to use only one GPU, please set num_gpus=0 and specify the GPU idx on run. example:
-		#expample 1 GPU of index 2 (train on "/gpu2" only): CUDA_VISIBLE_DEVICES=2 python train.py --model='Tacotron' --hparams='tacotron_gpu_start_idx=2'
-	#If you want to train on multiple GPUs, simply specify the number of GPUs available, and the idx of the first GPU to use. example:
-		#example 4 GPUs starting from index 0 (train on "/gpu0"->"/gpu3"): python train.py --model='Tacotron' --hparams='tacotron_num_gpus=4, tacotron_gpu_start_idx=0'
-	#The hparams arguments can be directly modified on this hparams.py file instead of being specified on run if preferred!
 
-	#If one wants to train both Tacotron and WaveNet in parallel (provided WaveNet will be trained on True mel spectrograms), one needs to specify different GPU idxes.
-	#example Tacotron+WaveNet on a machine with 4 or more GPUs. Two GPUs for each model: 
-		# CUDA_VISIBLE_DEVICES=0,1 python train.py --model='Tacotron' --hparams='tacotron_num_gpus=2'
-		# Cuda_VISIBLE_DEVICES=2,3 python train.py --model='WaveNet' --hparams='wavenet_num_gpus=2'
-
-	#IMPORTANT NOTES: The Multi-GPU performance highly depends on your hardware and optimal parameters change between rigs. Default are optimized for servers.
-	#If using N GPUs, please multiply the tacotron_batch_size by N below in the hparams! (tacotron_batch_size = 32 * N)
-	#Never use lower batch size than 32 on a single GPU!
-	#Same applies for Wavenet: wavenet_batch_size = 8 * N (wavenet_batch_size can be smaller than 8 if GPU is having OOM, minimum 2)
-	#Please also apply the synthesis batch size modification likewise. (if N GPUs are used for synthesis, minimal batch size must be N, minimum of 1 sample per GPU)
-	#We did not add an automatic multi-GPU batch size computation to avoid confusion in the user's mind and to provide more control to the user for
-	#resources related decisions.
-
-	#Acknowledgement:
-	#	Many thanks to @MlWoo for his awesome work on multi-GPU Tacotron which showed to work a little faster than the original
-	#	pipeline for a single GPU as well. Great work!
-
-	#Hardware setup: Default supposes user has only one GPU: "/gpu:0" (Both Tacotron and WaveNet can be trained on multi-GPU: data parallelization)
-	#Synthesis also uses the following hardware parameters for multi-GPU parallel synthesis.
-	'tacotron_num_gpus' : 1, #Determines the number of gpus in use for Tacotron training.
-	'wavenet_num_gpus' : 1, #Determines the number of gpus in use for WaveNet training.
-	'split_on_cpu' : True, #Determines whether to split data on CPU or on first GPU. This is automatically True when more than 1 GPU is used. 
-		#(Recommend: False on slow CPUs/Disks, True otherwise for small speed boost)
-	
 	#Audio
-	'num_mels' : 80, 
-	'num_freq' : 513, #only used when adding linear spectrograms post processing network
-	'rescale' : True, 
-	'rescaling_max' : 0.999,
-	'trim_silence' : True,
+	num_mels = 80, 
+	num_freq = 513, #only used when adding linear spectrograms post processing network
+	rescale = True, 
+	rescaling_max = 0.999,
+	trim_silence = True,
 
 	#Mel spectrogram
-	'fft_size' : 1024,
-	'hop_size' : 256,
-	'sample_rate' : 22050, #22050 Hz (corresponding to ljspeech dataset)
-	'frame_shift_ms' : None,
+	fft_size = 1024,
+	hop_size = 256,
+	sample_rate = 22050, #22050 Hz (corresponding to ljspeech dataset)
+	frame_shift_ms = None,
 
 	#Mel and Linear spectrograms normalization/scaling and clipping
-	'signal_normalization' : True,
-	'allow_clipping_in_normalization' : True, #Only relevant if mel_normalization' : True
-	'symmetric_mels' : True, #Whether to scale the data to be symmetric around 0
-	'max_abs_value' : 4., #max absolute value of data. If symmetric, data will be [-max, max] else [0, max] 
+	signal_normalization = True,
+	allow_clipping_in_normalization = True, #Only relevant if mel_normalization = True
+	symmetric_mels = True, #Whether to scale the data to be symmetric around 0
+	max_abs_value = 4., #max absolute value of data. If symmetric, data will be [-max, max] else [0, max] 
 
 	#Limits
-	'min_level_db' : -100,
-	'ref_level_db' : 20,
-	'fmin' : 125,
-	'fmax' : 8192,
+	min_level_db =- 100,
+	ref_level_db = 20,
+	fmin = 125,
+	fmax = 8192,
 
 	#Griffin Lim
-	'power' : 1.55,
-	'griffin_lim_iters' : 60,
+	power = 1.55,
+	griffin_lim_iters = 60,
 
 
 	#Tacotron
-	'outputs_per_step' : 1, #number of frames to generate at each decoding step (speeds up computation and allows for higher batch size)
-	'stop_at_any' : True, #Determines whether the decoder should stop when predicting <stop> to any frame or to all of them
+	outputs_per_step = 1, #number of frames to generate at each decoding step (speeds up computation and allows for higher batch size)
+	stop_at_any = True, #Determines whether the decoder should stop when predicting <stop> to any frame or to all of them
 
-	'embedding_dim' : 512, #dimension of embedding space
+	embedding_dim = 512, #dimension of embedding space
 
-	'enc_conv_num_layers' : 3, #number of encoder convolutional layers
-	'enc_conv_kernel_size' : (5, ), #size of encoder convolution filters for each layer
-	'enc_conv_channels' : 512, #number of encoder convolutions filters for each layer
-	'encoder_lstm_units' : 256, #number of lstm units for each direction (forward and backward)
+	enc_conv_num_layers = 3, #number of encoder convolutional layers
+	enc_conv_kernel_size = (5, ), #size of encoder convolution filters for each layer
+	enc_conv_channels = 512, #number of encoder convolutions filters for each layer
+	encoder_lstm_units = 256, #number of lstm units for each direction (forward and backward)
 
-	'smoothing' : False, #Whether to smooth the attention normalization function 
-	'attention_dim' : 128, #dimension of attention space
-	'attention_filters' : 32, #number of attention convolution filters
-	'attention_kernel' : (31, ),#kernel size of attention convolution
-	'cumulative_weights' : True, #Whether to cumulate (sum) all previous attention weights or simply feed previous weights (Recommended: True)
+	smoothing = False, #Whether to smooth the attention normalization function 
+	attention_dim = 128, #dimension of attention space
+	attention_filters = 32, #number of attention convolution filters
+	attention_kernel = (31, ), #kernel size of attention convolution
+	cumulative_weights = True, #Whether to cumulate (sum) all previous attention weights or simply feed previous weights (Recommended: True)
 
-	'prenet_layers' : [256, 256], #number of layers and number of units of prenet
-	'decoder_layers' : 2, #number of decoder lstm layers
-	'decoder_lstm_units' : 1024, #number of decoder lstm units on each layer
-	'max_iters' : 2500, #Max decoder steps during inference (Just for safety from infinite loop cases)
+	prenet_layers = [256, 256], #number of layers and number of units of prenet
+	decoder_layers = 2, #number of decoder lstm layers
+	decoder_lstm_units = 1024, #number of decoder lstm units on each layer
+	max_iters = 2500, #Max decoder steps during inference (Just for safety from infinite loop cases)
 
-	'postnet_num_layers' : 5, #number of postnet convolutional layers
-	'postnet_kernel_size' : (5, ), #size of postnet convolution filters for each layer
-	'postnet_channels' : 512, #number of postnet convolution filters for each layer
+	postnet_num_layers = 5, #number of postnet convolutional layers
+	postnet_kernel_size = (5, ), #size of postnet convolution filters for each layer
+	postnet_channels = 512, #number of postnet convolution filters for each layer
 
-	'mask_encoder' : False, #whether to mask encoder padding while computing attention
-	'impute_finished' : False, #Whether to use loss mask for padded sequences
-	'mask_finished' : False, #Whether to mask alignments beyond the <stop_token> (False for debug, True for style)
+	mask_encoder = False, #whether to mask encoder padding while computing attention
+	impute_finished = False, #Whether to use loss mask for padded sequences
+	mask_finished = False, #Whether to mask alignments beyond the <stop_token> (False for debug, True for style)
 
-	'predict_linear' : False, #Whether to add a post-processing network to the Tacotron to predict linear spectrograms (True mode Not tested!!)
+	predict_linear = False, #Whether to add a post-processing network to the Tacotron to predict linear spectrograms (True mode Not tested!!)
 
 
 	#Wavenet
@@ -144,37 +83,37 @@ hparams = Map({
 	# **NOTE**: if you change the one of the two parameters below, you need to
 	# re-run preprocessing before training.
 	# **NOTE**: scaler input (raw or mulaw) is experimental. Use it your own risk.
-	'input_type' : "mulaw-quantize",
-	'quantize_channels' : 256,  # 65536 or 256
+	input_type="mulaw-quantize",
+	quantize_channels=256,  # 65536 or 256
 
-	'silence_threshold' : 2,
+	silence_threshold=2,
 
 	# Mixture of logistic distributions:
-	'log_scale_min' : float(np.log(1e-14)),
+	log_scale_min=float(np.log(1e-14)),
 
 	#TODO model params
 
 
 	#Tacotron Training
-	'tacotron_batch_size' : 8,#32, #number of training samples on each training steps
-	'tacotron_reg_weight' : 1e-6, #regularization weight (for l2 regularization)
-	'tacotron_scale_regularization' : True, #Whether to rescale regularization weight to adapt for outputs range (used when reg_weight is high and biasing the model)
+	tacotron_batch_size = 8, #32, #number of training samples on each training steps
+	tacotron_reg_weight = 1e-6, #regularization weight (for l2 regularization)
+	tacotron_scale_regularization = True, #Whether to rescale regularization weight to adapt for outputs range (used when reg_weight is high and biasing the model)
 
-	'tacotron_decay_learning_rate' : True, #boolean, determines if the learning rate will follow an exponential decay
-	'tacotron_start_decay' : 50000, #Step at which learning decay starts
-	'tacotron_decay_steps' : 50000, #starting point for learning rate decay (and determines the decay slope) (UNDER TEST)
-	'tacotron_decay_rate' : 0.4, #learning rate decay rate (UNDER TEST)
-	'tacotron_initial_learning_rate' : 1e-3, #starting learning rate
-	'tacotron_final_learning_rate' : 1e-5, #minimal learning rate
+	tacotron_decay_learning_rate = True, #boolean, determines if the learning rate will follow an exponential decay
+	tacotron_start_decay = 50000, #Step at which learning decay starts
+	tacotron_decay_steps = 50000, #starting point for learning rate decay (and determines the decay slope) (UNDER TEST)
+	tacotron_decay_rate = 0.4, #learning rate decay rate (UNDER TEST)
+	tacotron_initial_learning_rate = 1e-3, #starting learning rate
+	tacotron_final_learning_rate = 1e-5, #minimal learning rate
 
-	'tacotron_adam_beta1' : 0.9, #AdamOptimizer beta1 parameter
-	'tacotron_adam_beta2' : 0.999, #AdamOptimizer beta2 parameter
-	'tacotron_adam_epsilon' : 1e-6, #AdamOptimizer beta3 parameter
+	tacotron_adam_beta1 = 0.9, #AdamOptimizer beta1 parameter
+	tacotron_adam_beta2 = 0.999, #AdamOptimizer beta2 parameter
+	tacotron_adam_epsilon = 1e-6, #AdamOptimizer beta3 parameter
 
-	'tacotron_zoneout_rate' : 0.1, #zoneout rate for all LSTM cells in the network
-	'tacotron_dropout_rate' : 0.5, #dropout rate for all convolutional layers + prenet
+	tacotron_zoneout_rate = 0.1, #zoneout rate for all LSTM cells in the network
+	tacotron_dropout_rate = 0.5, #dropout rate for all convolutional layers + prenet
 
-	'tacotron_teacher_forcing_ratio' : 1., #Value from [0., 1.], 0.=0%, 1.=100%, determines the % of times we force next decoder inputs
+	tacotron_teacher_forcing_ratio = 1., #Value from [0., 1.], 0.=0%, 1.=100%, determines the % of times we force next decoder inputs
 	
 
 	#Wavenet Training TODO
@@ -182,7 +121,7 @@ hparams = Map({
 
 
 	#Eval sentences
-	'sentences' : [
+	sentences = [
 	# From July 8, 2017 New York Times:
 	'Scientists at the CERN laboratory say they have discovered a new particle.',
 	'There\'s a way to measure the acute emotional intelligence that has never gone out of style.',
@@ -222,8 +161,9 @@ hparams = Map({
 	'Thank you so much for your support!!'
 	]
 
-})
+	)
 
 def hparams_debug_string():
-	hp = ['  %s: %s' % (name, value) for name, value in hparams.items() if name != 'sentences']
+	values = hparams.values()
+	hp = ['  %s: %s' % (name, values[name]) for name in sorted(values) if name != 'sentences']
 	return 'Hyperparameters:\n' + '\n'.join(hp)
